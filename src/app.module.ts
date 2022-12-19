065aa946664dd4bd82b86df3ccb8a96ec2dd8ef8
HermanoCastro65 <hermanocastro18@gmail.com>
2022-12-18 23:08:04 -0300

    create envroiment variable and conect with mongo db cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common'
 import { UsersModule } from './users/users.module'
+import { MongooseModule } from '@nestjs/mongoose'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const password = process.env.PASSWORD
 
 @Module({
-  imports: [UsersModule],
+  imports: [MongooseModule.forRoot(password), UsersModule],
   controllers: [],
   providers: [],
 })
